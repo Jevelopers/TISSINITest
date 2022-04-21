28d0c8ae376de30f18c9138425adab8e0388ea6e
Jevelopers <jwchavarro@corhuila.edu.co>
2022-04-21 14:51:40 -0500

    UpdateFormLabel

--- tissini/src/Components/Formulario.jsx
+++ tissini/src/Components/Formulario.jsx
@@ -3,11 +3,15 @@ import stylesFormulario from '../Components/Formulario.module.css'
 
 export default function Formulario() {
   return (
-    <div className= {stylesFormulario.container}>
-        <form>
-            <div className= {stylesFormulario.group}></div>
-          <input type="text" className= {stylesFormulario.input}  placeholder='ododosd' required />
-      </form>
-    </div>
+    <form className={stylesFormulario.form}>
+      <div className={stylesFormulario.form__container}>
+        <div className={stylesFormulario.form__group}>
+          <input type="number" className={stylesFormulario.form__input} id="codeNumber" placeholder='' required />
+          <label className={stylesFormulario.form__label}>Teléfono:</label>
+        </div>
+        <input type="submit" className={stylesFormulario.form__submit} value="Entrar" />
+
+      </div>
+    </form>
   )
 }
